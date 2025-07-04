cf5aa1294fb3106221912b4f8f85e47b5e4fbf17
Aashish Gaurav <aashishgaurav175@gmail.com>
2025-07-05 01:43:48 +0530

    Token done

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import dotenv from 'dotenv'
 import connectDB from "./db/index.js";
+import { app } from './app..js';
 // import express from 'express'
 
 // const app = express()
@@ -24,4 +25,12 @@ import connectDB from "./db/index.js";
 dotenv.config({
   path: './env'
 })
-connectDB();
+connectDB()
+.then(() =>{
+  app.listen(process.env.PORT || 8000, ()=>{
+    console.log(`app is running on port ${process.env.PORT}`)
+  })
+})
+.catch((err)=>{
+  console.log('We caught an error : ', err)
+})
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,20 @@
+const asyncHandler = (requestHandler) =>{
+ (req, res, next) => {
+  Promise.resolve(requestHandler(req, res, next))
+  .catch((err) => next(err))
+ }
+}
+
+export default asyncHandler
+
+// const asyncHandler = () => async (req, res, next) =>{
+//   try{
+
+//   }
+//   catch(err){
+//      res.status(err.code || 500).json({
+//       success: false,
+//       message: err.message
+//      })
+//   }
+// }
